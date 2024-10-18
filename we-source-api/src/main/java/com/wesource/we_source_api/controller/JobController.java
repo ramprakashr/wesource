b1698016f9852caed6cadd96dea46db7365926cb
ramprakashr <rramprakash1@gmail.com>
2024-10-18 23:34:36 +0530

    Add Job Model class

---
+++ we-source-api/src/main/java/com/wesource/we_source_api/controller/JobController.java
@@ -0,0 +1,14 @@
+package com.wesource.we_source_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/job")
+public class JobController {
+	
+	public void createNewJob() {
+		
+	}
+
+}
---
+++ we-source-api/src/main/java/com/wesource/we_source_api/model/Job.java
@@ -0,0 +1,132 @@
+package com.wesource.we_source_api.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "ws_job")
+public class Job {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Integer ws_job_id;
+	
+	private String ws_job_title;
+	private Date ws_job_from_date;
+	private Date ws_job_to_date;
+	private JobPeriod ws_job_period;
+	private JobCategory ws_job_category;
+	private JobStatus ws_job_status;
+	private Float ws_job_proposed_fees;
+	private Float ws_job_final_fees;
+	private Integer ws_job_fee_interval;
+	private String ws_job_description;
+	private char ws_job_is_wfh;
+	private Integer ws_job_created_by;
+	private Date ws_job_created_date;
+	private Integer ws_job_last_updated_by;
+	private Date ws_job_last_updated_date;
+	
+	public Integer getWs_job_id() {
+		return ws_job_id;
+	}
+	public void setWs_job_id(Integer ws_job_id) {
+		this.ws_job_id = ws_job_id;
+	}
+	public String getWs_job_title() {
+		return ws_job_title;
+	}
+	public void setWs_job_title(String ws_job_title) {
+		this.ws_job_title = ws_job_title;
+	}
+	public Date getWs_job_from_date() {
+		return ws_job_from_date;
+	}
+	public void setWs_job_from_date(Date ws_job_from_date) {
+		this.ws_job_from_date = ws_job_from_date;
+	}
+	public Date getWs_job_to_date() {
+		return ws_job_to_date;
+	}
+	public void setWs_job_to_date(Date ws_job_to_date) {
+		this.ws_job_to_date = ws_job_to_date;
+	}
+	public JobPeriod getWs_job_period() {
+		return ws_job_period;
+	}
+	public void setWs_job_period(JobPeriod ws_job_period) {
+		this.ws_job_period = ws_job_period;
+	}
+	public JobCategory getWs_job_category() {
+		return ws_job_category;
+	}
+	public void setWs_job_category(JobCategory ws_job_category) {
+		this.ws_job_category = ws_job_category;
+	}
+	public JobStatus getWs_job_status() {
+		return ws_job_status;
+	}
+	public void setWs_job_status(JobStatus ws_job_status) {
+		this.ws_job_status = ws_job_status;
+	}
+	public Float getWs_job_proposed_fees() {
+		return ws_job_proposed_fees;
+	}
+	public void setWs_job_proposed_fees(Float ws_job_proposed_fees) {
+		this.ws_job_proposed_fees = ws_job_proposed_fees;
+	}
+	public Float getWs_job_final_fees() {
+		return ws_job_final_fees;
+	}
+	public void setWs_job_final_fees(Float ws_job_final_fees) {
+		this.ws_job_final_fees = ws_job_final_fees;
+	}
+	public Integer getWs_job_fee_interval() {
+		return ws_job_fee_interval;
+	}
+	public void setWs_job_fee_interval(Integer ws_job_fee_interval) {
+		this.ws_job_fee_interval = ws_job_fee_interval;
+	}
+	public String getWs_job_description() {
+		return ws_job_description;
+	}
+	public void setWs_job_description(String ws_job_description) {
+		this.ws_job_description = ws_job_description;
+	}
+	public char getWs_job_is_wfh() {
+		return ws_job_is_wfh;
+	}
+	public void setWs_job_is_wfh(char ws_job_is_wfh) {
+		this.ws_job_is_wfh = ws_job_is_wfh;
+	}
+	public Integer getWs_job_created_by() {
+		return ws_job_created_by;
+	}
+	public void setWs_job_created_by(Integer ws_job_created_by) {
+		this.ws_job_created_by = ws_job_created_by;
+	}
+	public Date getWs_job_created_date() {
+		return ws_job_created_date;
+	}
+	public void setWs_job_created_date(Date ws_job_created_date) {
+		this.ws_job_created_date = ws_job_created_date;
+	}
+	public Integer getWs_job_last_updated_by() {
+		return ws_job_last_updated_by;
+	}
+	public void setWs_job_last_updated_by(Integer ws_job_last_updated_by) {
+		this.ws_job_last_updated_by = ws_job_last_updated_by;
+	}
+	public Date getWs_job_last_updated_date() {
+		return ws_job_last_updated_date;
+	}
+	public void setWs_job_last_updated_date(Date ws_job_last_updated_date) {
+		this.ws_job_last_updated_date = ws_job_last_updated_date;
+	}
+	
+}
